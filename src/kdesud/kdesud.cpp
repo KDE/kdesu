5de62b90fdaece5555965876af8f991d48640f21
Lukáš Tinkl <lukas@kde.org>
2014-11-12 20:16:59 +0100

    Q_WS_X11 -> HAVE_X11
    the former is void, therefore the code would never be compiled

--- src/kdesud/kdesud.cpp
+++ src/kdesud/kdesud.cpp
@@ -73,10 +73,9 @@
 #include "repo.h"
 #include "handler.h"
 
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
 #include <X11/X.h>
 #include <X11/Xlib.h>
-#include <QStandardPaths>
 #endif
 
 #ifndef SUN_LEN
@@ -95,7 +94,7 @@ using namespace KDESu;
 Repository *repo;
 QString Version(QStringLiteral("1.01"));
 QByteArray sock;
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
 Display *x11Display;
 #endif
 int pipeOfDeath[2];
@@ -108,7 +107,7 @@ void kdesud_cleanup()
 
 // Borrowed from kdebase/kaudio/kaudioserver.cpp
 
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
 extern "C" int xio_errhandler(Display *);
 
 int xio_errhandler(Display *)
@@ -308,7 +307,7 @@ int main(int argc, char *argv[])
     if (pid)
         _exit(0);
 
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
     // Make sure we exit when the display gets closed.
     int x11Fd = initXconnection();
     maxfd = qMax(maxfd, x11Fd);
@@ -345,7 +344,7 @@ int main(int argc, char *argv[])
     FD_ZERO(&active_fds);
     FD_SET(sockfd, &active_fds);
     FD_SET(pipeOfDeath[0], &active_fds);
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
     if (x11Fd != -1)
         FD_SET(x11Fd, &active_fds);
 #endif
@@ -353,7 +352,7 @@ int main(int argc, char *argv[])
     while (1)
     {
         tmp_fds = active_fds;
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
         if(x11Display)
             XFlush(x11Display);
 #endif
@@ -397,7 +396,7 @@ int main(int argc, char *argv[])
                 while(result > 0);
             }
 
-#ifdef Q_WS_X11
+#ifdef HAVE_X11
             if (i == x11Fd)
             {
                 // Discard X events
--- src/kdesud/secure.cpp
+++ src/kdesud/secure.cpp
@@ -66,7 +66,7 @@ SocketSecurity::SocketSecurity(int sockfd) : pid(-1), gid(-1), uid(-1)
     ucred cred;
     ksocklen_t len = sizeof(struct ucred);
     if (getsockopt(sockfd, SOL_SOCKET, SO_PEERCRED, &cred, &len) < 0) {
-	qCritical() << "getsockopt(SO_PEERCRED) " << perror << endl;
+        qCritical() << "getsockopt(SO_PEERCRED) " << perror << endl;
 	return;
     }
     pid = cred.pid;
